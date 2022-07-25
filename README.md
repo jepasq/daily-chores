65a9c2fd2a50d992df3793bc4a8f1f2a2fb20986
Jepasq <jepasq111@gmail.com>
2022-07-25 10:01:51 +0200

    Add mocha dependency and test script

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ An electron application to check for daily chores.
 
 	npm start
 
+# Unit tests
+
+	npm test
+
 # License
 
 `daily-chores` is licensed under GPL v3 or later.
---
+++ test/chore.test.js
@@ -0,0 +1,20 @@
+const { Chore, ChoreTemplate} = require('../src/chore');
+const assert = require('assert');
+
+
+describe('ChoreTemplate Test', () => {
+    it('can be instantiated', () => {
+	let ct = new ChoreTemplate();
+        assert.not_equal(ct, null);
+    });
+
+});
+
+describe('Simple Math Test', () => {
+ it('should return 2', () => {
+        assert.equal(1 + 1, 2);
+ });
+ it('should return 9', () => {
+        assert.equal(3 * 3, 9);
+ });
+});
