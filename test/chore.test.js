a69aa2dcd52fde6b9f1c3f753ef010481bdf3af5
Jepasq <jepasq111@gmail.com>
2022-10-31 09:43:15 +0100

    Remove math-based example test

--- test/chore.test.js
+++ test/chore.test.js
@@ -36,12 +36,3 @@ describe('ChoreTemplate Test', () => {
 
     
 });
-
-describe('Simple Math Test', () => {
-    it('should return 2', () => {
-        assert.equal(1 + 1, 2);
-    });
-    it('should return 9', () => {
-        assert.equal(3 * 3, 9);
-    });
-});
