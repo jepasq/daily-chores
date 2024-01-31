f25157ebbb4599f4ce8f880fc3a1f14794feffbc
Jepasq <jepasq111@gmail.com>
2024-01-31 13:15:43 +0100

    Fix fake local storage missing in new chore template implementation

--- test/chore-template.test.js
+++ test/chore-template.test.js
@@ -2,9 +2,18 @@ const assert = require('assert');
 
 const {ChoreTemplate} = require('../src/chore-template');
 
+/// A fake LocalStorage item
+var ls = {
+    getItem: function (key) {
+        if( key === 'id_token' ){ return /* a token object */; }
+        return null;
+    },
+    setItem: function (key) { }
+}
+
 describe('ChoreTemplate Test', () => {
     it('can be instantiated', () => {
-	let ct = new ChoreTemplate();
+	let ct = new ChoreTemplate(ls);
         assert.notEqual(ct, null);
 	
     });
